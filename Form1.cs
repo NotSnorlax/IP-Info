ec70d724e93fdb1214caea7e9aaf7c0163e88ad4
FikuSystems <GabrielKo2008@outlook.com>
2024-05-02 11:44:04 +0100

    added error handling to copying text, added false to display on anycast if not present.

--- Form1.cs
+++ Form1.cs
@@ -55,6 +55,10 @@ namespace IPgrab
 					Data info = JsonConvert.DeserializeObject<Data>(mesData);
 					ip.Text = info.ip;
 					anycast.Text = info.anycast;
+                    if (anycast.Text == "")
+                    {
+                        anycast.Text = "False";
+                    }
 					city.Text = info.city;
 					region.Text = info.region;
 					country.Text = info.country;
@@ -63,6 +67,7 @@ namespace IPgrab
 					postal.Text = info.postal;
 					timezone.Text = info.timezone;
                     readme.Text = info.readme;
+
 				}
 				label2.Text = "Grabbed: " + textBox1.Text;
                 textBox1.Enabled = true;
@@ -83,57 +88,163 @@ namespace IPgrab
 
         private void button4_Click(object sender, EventArgs e)
         {
-            System.Windows.Forms.Clipboard.SetText(ip.Text);
+            try
+            {
+                System.Windows.Forms.Clipboard.SetText(ip.Text);
+            }
+            catch
+            {
+                if (!ip.Text.EndsWith(" - Can't Copy"))
+                {
+                    ip.Text = ip.Text + " - Can't Copy";
+                }
+            }
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
+            try { 
             System.Windows.Forms.Clipboard.SetText(anycast.Text);
+            }
+            catch
+            {
+                if (!anycast.Text.EndsWith(" - Can't Copy"))
+                {
+                    anycast.Text = anycast.Text + " - Can't Copy";
+                }
+            }
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            System.Windows.Forms.Clipboard.SetText(city.Text);
+            try
+            {
+                System.Windows.Forms.Clipboard.SetText(city.Text);
+            }
+            catch
+            {
+                if (!city.Text.EndsWith(" - Can't Copy"))
+                {
+                    city.Text = city.Text + " - Can't Copy";
+                }
+            }
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            System.Windows.Forms.Clipboard.SetText(region.Text);
+                try
+                {
+                    System.Windows.Forms.Clipboard.SetText(region.Text);
+            }
+            catch
+            {
+                if (!region.Text.EndsWith(" - Can't Copy"))
+                {
+                    region.Text = region.Text + " - Can't Copy";
+                }
+            }
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            System.Windows.Forms.Clipboard.SetText(country.Text);
+                    try
+                    {
+                        System.Windows.Forms.Clipboard.SetText(country.Text);
+            }
+            catch
+            {
+                if (!country.Text.EndsWith(" - Can't Copy"))
+                {
+                    country.Text = country.Text + " - Can't Copy";
+                }
+            }
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
-            System.Windows.Forms.Clipboard.SetText(loc.Text);
+                        try
+                        {
+                            System.Windows.Forms.Clipboard.SetText(loc.Text);
+            }
+            catch
+            {
+                if (!loc.Text.EndsWith(" - Can't Copy"))
+                {
+                    loc.Text = loc.Text + " - Can't Copy";
+                }
+            }
         }
 
         private void button11_Click(object sender, EventArgs e)
         {
-            System.Windows.Forms.Clipboard.SetText(org.Text);
+                            try
+                            {
+                                System.Windows.Forms.Clipboard.SetText(org.Text);
+            }
+            catch
+            {
+                if (!org.Text.EndsWith(" - Can't Copy"))
+                {
+                    org.Text = org.Text + " - Can't Copy";
+                }
+            }
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
-            System.Windows.Forms.Clipboard.SetText(postal.Text);
+                                try
+                                {
+                                    System.Windows.Forms.Clipboard.SetText(postal.Text);
+            }
+            catch
+            {
+                if (!postal.Text.EndsWith(" - Can't Copy"))
+                {
+                    postal.Text = postal.Text + " - Can't Copy";
+                }
+            }
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-            System.Windows.Forms.Clipboard.SetText(timezone.Text);
+                                    try
+                                    {
+                                        System.Windows.Forms.Clipboard.SetText(timezone.Text);
+            }
+            catch
+            {
+                if (!timezone.Text.EndsWith(" - Can't Copy"))
+                {
+                    timezone.Text = timezone.Text + " - Can't Copy";
+                }
+            }
         }
 
         private void button13_Click(object sender, EventArgs e)
         {
-            System.Windows.Forms.Clipboard.SetText(readme.Text);
+                                        try
+                                        {
+                                            System.Windows.Forms.Clipboard.SetText(readme.Text);
+            }
+            catch
+            {
+                if (!readme.Text.EndsWith(" - Can't Copy"))
+                {
+                    readme.Text = readme.Text + " - Can't Copy";
+                }
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            System.Windows.Forms.Clipboard.SetText("IP: " + ip.Text + "\r\nAnycast: " + anycast.Text + "\r\nCity: " + city.Text + "\r\nRegion: " + region.Text + "\r\nCountry: " + country.Text + "\r\nLoc: " + loc.Text + "\r\nOrg: " + org.Text + "\r\nPostal: " + postal.Text + "\r\nTimezone: " + timezone.Text + "\r\nReadme:" + readme.Text);
+                                            try
+                                            {
+                                                System.Windows.Forms.Clipboard.SetText("IP: " + ip.Text + "\r\nAnycast: " + anycast.Text + "\r\nCity: " + city.Text + "\r\nRegion: " + region.Text + "\r\nCountry: " + country.Text + "\r\nLoc: " + loc.Text + "\r\nOrg: " + org.Text + "\r\nPostal: " + postal.Text + "\r\nTimezone: " + timezone.Text + "\r\nReadme:" + readme.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Cannot copy all" + ex, "IP Info - Error");
+            }
         }
 
         private void Form1_HelpButtonClicked(object sender, CancelEventArgs e)
